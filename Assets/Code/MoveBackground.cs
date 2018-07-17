8c7ebd9cefcc2d8c868ab02003062b49e029b05c
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-07-17 02:58:47 -0300

    Clouds move

---
+++ Assets/Code/MoveBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    private float Speed;
+
+    void Start()
+    {
+        Speed = Random.Range(0.1F, 0.5f);
+    }
+    
+    void Update()
+    {
+        this.transform.position = new Vector2(this.transform.position.x + Speed * Time.deltaTime,
+            this.transform.position.y);
+    }
+
+    void OnInvisible()
+    {
+        this.transform.position = new Vector2(-8f, this.transform.position.y);
+        Speed = Random.Range(0.1F, 0.5f);
+    }
+}
