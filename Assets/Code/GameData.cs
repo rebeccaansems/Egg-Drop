448d52c395ebff416c2ddcbbcc20b7bdaef08791
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-07-21 21:49:06 -0300

    Changed text sizes on stats screen

--- Assets/Code/GameData.cs
+++ Assets/Code/GameData.cs
@@ -6,22 +6,31 @@ using UnityEngine.UI;
 
 public class GameData : MonoBehaviour
 {
+    public static List<Tuple<int, int>> l_HighScores;
+
     public static List<GameObject> l_AllEggs;
+
     public static int k_Score, k_Bounces, k_Eggs, k_GameNumber;
     public static int k_EggsAllTime, k_BouncesAllTime;
-    public static bool k_GameRunning = true;
+    public static bool k_GameRunning = true, k_ScoresUpdated;
 
     public GameObject EndGamePanel;
     public Text ScoreText;
 
     void Start()
     {
+        k_ScoresUpdated = false;
         k_Score = 0;
         k_Bounces = 0;
-        k_Eggs = 1; 
+        k_Eggs = 1;
         k_GameNumber = PlayerPrefs.GetInt("GameNumber", 1);
         k_BouncesAllTime = PlayerPrefs.GetInt("Bounces", 0);
         k_EggsAllTime = PlayerPrefs.GetInt("Eggs", 0);
+
+        l_HighScores = new List<Tuple<int, int>>();
+        l_HighScores.Add(new Tuple<int, int>(PlayerPrefs.GetInt("Highscore_Date0", 0), PlayerPrefs.GetInt("Highscore_Score0", 0)));
+        l_HighScores.Add(new Tuple<int, int>(PlayerPrefs.GetInt("Highscore_Date1", 0), PlayerPrefs.GetInt("Highscore_Score1", 0)));
+        l_HighScores.Add(new Tuple<int, int>(PlayerPrefs.GetInt("Highscore_Date2", 0), PlayerPrefs.GetInt("Highscore_Score2", 0)));
 
         Time.timeScale = 1;
 
