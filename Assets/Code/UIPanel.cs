81cd80d7fe84345e80994045d37f0c8492dc71ad
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-10 23:28:42 -0300

    Changed twitter share text

--- Assets/Code/UIPanel.cs
+++ Assets/Code/UIPanel.cs
@@ -52,11 +52,11 @@ public class UIPanel : MonoBehaviour
     {
         SocialShare = new SocialShareSheet();
 #if UNITY_IOS
-        SocialShare.URL = "https://itunes.apple.com/us/app/f-in-the-b/id1328718409?ls=1&mt=8";
+        SocialShare.URL = "https://itunes.apple.com/us/app/dont-drop-the-egg/id1435852144?ls=1&mt=8";
 #elif UNITY_ANDROID
-        SocialShare.URL = "https://play.google.com/store/apps/details?id=com.rebeccaansems.fintheb";
+        SocialShare.URL = "https://play.google.com/store/apps/details?id=com.rebeccaansems.dontdroptheegg";
 #endif
-        SocialShare.Text = "My highscore on Egg Drop is " + GameData.l_HighScores[0].Second + " can you beat it? " + SocialShare.URL;
+        SocialShare.Text = "My highscore on Don't Drop the Egg is " + GameData.l_HighScores[0].Second + " can you beat it? " + SocialShare.URL;
 
         // Show composer
         NPBinding.UI.SetPopoverPointAtLastTouchPosition(); // To show popover at last touch point on iOS. On Android, its ignored.
