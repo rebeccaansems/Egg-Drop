47a2e286aaa53555e8f6341400aeeb1be71491b6
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-09-12 23:19:19 -0300

    Fixed play menu from reshowing

--- Assets/Code/UIGameStart.cs
+++ Assets/Code/UIGameStart.cs
@@ -11,16 +11,17 @@ public class UIGameStart : MonoBehaviour
 
         if (GameData.k_GameInSessionNumber != 0)
         {
-            Time.timeScale = 1;
+            this.GetComponent<Animator>().SetInteger("gameStarted", 2);
             this.GetComponent<CanvasGroup>().alpha = 0;
             this.GetComponent<CanvasGroup>().interactable = false;
             this.GetComponent<CanvasGroup>().blocksRaycasts = false;
+            Time.timeScale = 1;
         }
     }
 
     public void StartGame()
     {
-        this.GetComponent<Animator>().SetBool("gameStarted", true);
+        this.GetComponent<Animator>().SetInteger("gameStarted", 1);
         this.GetComponent<CanvasGroup>().interactable = false;
         this.GetComponent<CanvasGroup>().blocksRaycasts = false;
         Time.timeScale = 1;
